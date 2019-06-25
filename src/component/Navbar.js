d0514660bd2cfe0b33c577773e2bdbd5746a8147
Bgiris <berkaygiris@gmail.com>
2019-06-25 23:48:28 +0300

    feat: creates healthpage

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   state = {};
@@ -11,21 +12,25 @@ export default class Navbar extends Component {
 
     return (
       <Menu>
-        <Menu.Item
-          name="home"
-          active={activeItem === "home"}
-          onClick={this.handleItemClick}
-        >
-          Home
-        </Menu.Item>
+        <Link to="/">
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          >
+            Home
+          </Menu.Item>
+        </Link>
 
-        <Menu.Item
-          name="health"
-          active={activeItem === "health"}
-          onClick={this.handleItemClick}
-        >
-          Health
-        </Menu.Item>
+        <Link to="/health">
+          <Menu.Item
+            name="health"
+            active={activeItem === "health"}
+            onClick={this.handleItemClick}
+          >
+            Health
+          </Menu.Item>
+        </Link>
       </Menu>
     );
   }
---
+++ src/pages/HealthPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Health from "./../component/Health";
+import "./../App.css";
+
+function HealthPage() {
+  return <Health />;
+}
+
+export default HealthPage;
